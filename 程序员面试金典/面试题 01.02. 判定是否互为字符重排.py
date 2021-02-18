eb978e8a231835a1282bd0236bdb5269154e042a
董光文 <879967992@qq.con>
2021-02-18 11:59:36 +0800

    面试题 01.02. 判定是否互为字符重排

--- 程序员面试金典/面试题 01.02. 判定是否互为字符重排.py
+++ 程序员面试金典/面试题 01.02. 判定是否互为字符重排.py
@@ -12,21 +12,31 @@
 输出: false
 """
 #  方法一（位运算）：
-class Solution(object):
-    def CheckPermutation(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
-        if len(s1) != len(s2):
-            return False
-        res = 0
-        for i in range(len(s1)):
-            res += 1 << ord(s1[i])
-            res -= 1 << ord(s2[i])
-        return res == 0
+# class Solution(object):
+#     def CheckPermutation(self, s1, s2):
+#         """
+#         :type s1: str
+#         :type s2: str
+#         :rtype: bool
+#         """
+#         if len(s1) != len(s2):
+#             return False
+#         res = 0
+#         for i in range(len(s1)):
+#             res += 1 << ord(s1[i])
+#             res -= 1 << ord(s2[i])
+#         return res == 0
 #  方法二：字符串排序后比较
+# class Solution(object):
+#     def CheckPermutation(self, s1, s2):
+#         """
+#         :type s1: str
+#         :type s2: str
+#         :rtype: bool
+#         """
+#         return sorted(s1) == sorted(s2)
+
+#  方法三：利用集合元素的不可重复性
 class Solution(object):
     def CheckPermutation(self, s1, s2):
         """
@@ -34,9 +44,13 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        return sorted(s1) == sorted(s2)
+        return set(s1) == set(s2)
 
 
+a = Solution()
+# b = a.CheckPermutation('abc', 'bca')
+b = a.CheckPermutation('abd', 'bca')
+print(b)
 """
 思路：位运算
 时间复杂度： O(N)
