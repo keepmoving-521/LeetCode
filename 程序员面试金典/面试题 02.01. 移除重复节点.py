483470962944d1229be026b2cf2b3aa6f8cb6170
董光文 <879967992@qq.con>
2021-04-13 22:09:10 +0800

    面试题 02.01. 移除重复节点

--- 程序员面试金典/面试题 02.01. 移除重复节点.py
+++ 程序员面试金典/面试题 02.01. 移除重复节点.py
@@ -10,30 +10,57 @@
  输入：[1, 1, 1, 1, 2]
  输出：[1, 2]
 '''
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#  definition for a single_link list
+class LinkNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
-class Solution(object):
-    def removeDuplicateNodes(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        if not head:
-            return head
-        currented = {head.val}
-        pos = head
-        while pos.next:
-            cur = pos.next
-            if cur.val not in currented:
-                currented.add(cur.val)
-                pos = pos.next
-            else:
-                pos.next = pos.next.next
+
+def creat_nodelist(li):
+    if not li:
+        return
+    head = pos = LinkNode(li[0])
+    for i in li[1:]:
+        pos.next = LinkNode(i)
+        pos = pos.next
+    return head
+
+
+def print_nodelist(head):
+    while head:
+        print(head.val, end=' ')
+        head = head.next
+    print()
+
+
+def remove_duplicate_nodes(head):
+    if not head:
         return head
+    current_node = {head.val}
+    pos = head
+    while pos.next:
+        cur = pos.next
+        if cur.val not in current_node:
+            current_node.add(cur.val)
+            pos = pos.next
+        else:
+            pos.next = pos.next.next
+    return head
+
+
+#  test1
+node = [1, 2, 3, 3, 2, 1, 5]
+node_link = creat_nodelist(node)
+obj = remove_duplicate_nodes(node_link)
+print(obj)
+print_nodelist(obj)
+#  test2
+node2 = [1, 1, 1, 1, 2]
+node_link2 = creat_nodelist(node2)
+obj2 = remove_duplicate_nodes(node_link2)
+print(obj2)
+print_nodelist(obj2)
 
 """
 方法一：哈希表
