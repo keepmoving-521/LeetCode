82e802d35eb33b386e76f9e8b1f00d41b09f9a08
董光文 <879967992@qq.con>
2021-04-18 10:57:25 +0800

    面试题 02.03. 删除中间节点

--- 程序员面试金典/面试题 02.03. 删除中间节点.py
+++ 程序员面试金典/面试题 02.03. 删除中间节点.py
@@ -7,21 +7,55 @@
 输入：单向链表a->b->c->d->e->f中的节点c
 结果：不返回任何数据，但该链表变为a->b->d->e->f
 """
+
+
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class LinkNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution(object):
-    def deleteNode(self, node):
-        """
-        :type node: ListNode
-        :rtype: void Do not return anything, modify node in-place instead.
-        """
-        node.val = node.next.val
-        node.next = node.next.next
+    #  create a linklist
+    def creat_linklist(self, li):
+        if not li:
+            print('This is an empty list')
+            return
+        head = p = LinkNode(li[0])
 
+        for i in li[1:]:
+            p.next = LinkNode(i)
+            p = p.next
+        return head
+
+    def print_linklist(self, head):
+        if not head:
+            print('This is an empty list')
+            return
+        while head:
+            print(head.val, end="->")
+            head = head.next
+        print('None')
+
+    def del_node(self, head, node):
+        if not head:
+            print('This is an empty list')
+            return
+        while head:
+            if head.val == node:
+                head.val = head.next.val
+                head.next = head.next.next
+            head = head.next
+
+
+#  test
+li = ['a', 'b', 'c', 'd', 'e', 'f']
+obj = Solution()
+head = obj.creat_linklist(li)
+obj.print_linklist(head)
+obj.del_node(head, 'c')
+obj.print_linklist(head)
 
 '''
 这题的核心思想其实就是把node的下一位的值覆盖给node，然后跳过node的下一位
